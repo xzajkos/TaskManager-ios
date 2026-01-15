7aecff1ee23197a9e681b18af605aa413f327a7c
Kacper Zajkowski <kacperzajkowski@icloud.com>
2026-01-15 16:34:07 +0100

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Kacper Zajkowski on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
